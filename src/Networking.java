79b1dc71a27ca63bc66acc8756a7f2e333f648fd
Nicole Huang <nicoleh87@nycstudents.net>
2022-05-18 10:17:18 -0400

    Created Networking and Weather class, imported json library to help

--- src/Networking.java
+++ src/Networking.java
@@ -1,2 +1,39 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.json.JSONObject;
+
 public class Networking {
+    private final static String API_KEY = "30f89806b18649fc976133318221805";
+
+    public static Weather getCurrentWeather(String zipCode) {
+        final String url = "https://api.weatherapi.com/v1/current.json?key="+API_KEY+"&q="+zipCode+"&aqi=no";
+        String response = makeAPICall(url);
+
+        if (response == null) {
+            return null;
+        }
+
+        return parseJSON(response);
+    }
+
+    private static String makeAPICall(String url) {
+        try {
+            URI myUri = URI.create(url);
+            HttpRequest request = HttpRequest.newBuilder().uri(myUri).build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private static Weather parseJSON(String json) {
+        JSONObject jsonObject = new JSONObject(json);
+        return new Weather(jsonObject);
+    }
 }
--- src/Weather.java
+++ src/Weather.java
@@ -1,2 +1,34 @@
+import org.json.JSONObject;
+
 public class Weather {
+    private final double temperatureF;
+    private final double temperatureC;
+    private final String currentCondition;
+    private final String conditionIcon;
+
+    public Weather(JSONObject jsonObject) {
+        JSONObject current = jsonObject.getJSONObject("current");
+        JSONObject condition = jsonObject.getJSONObject("condition");
+
+        temperatureF = current.getDouble("temp_f");
+        temperatureC = current.getDouble("temp_c");
+        currentCondition = condition.getString("text");
+        conditionIcon = condition.getString("icon");
+    }
+
+    public double getTemperatureF() {
+        return temperatureF;
+    }
+
+    public double getTemperatureC() {
+        return temperatureC;
+    }
+
+    public String getCurrentCondition() {
+        return currentCondition;
+    }
+
+    public String getConditionIcon() {
+        return conditionIcon;
+    }
 }
